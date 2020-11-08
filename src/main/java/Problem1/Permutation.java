f86904ff43bb9420d21d851b58aa73059327b4f4
TrentLucas <itsstatiic@gmail.com>
2020-11-07 18:30:59 -0800

    everything works besides reverse()

--- src/main/java/Problem1/Permutation.java
+++ src/main/java/Problem1/Permutation.java
@@ -1,11 +1,41 @@
 package Problem1;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Permutation {
     public static List<List<Integer>> permute(int[] nums) {
         // homework
-        return new ArrayList<>(); // place holder
+
+        List<List<Integer>> result = new ArrayList<>();
+        permute(nums, 0, result);
+        return result;
+    }
+    private static void permute(int[] nums, int position, List<List<Integer>> result) {
+        //stopping condition
+        if (nums.length == 0) {
+            result.add(new ArrayList<>());
+        }
+        if (position == nums.length - 1) {
+            List<Integer> oneResult = new ArrayList<>();
+            for (int i = 0; i < nums.length; i++) {
+                oneResult.add(nums[i]);
+            }
+            result.add(oneResult);
+
+            return;
+        }
+        for (int i = position; i < nums.length; i++){
+            int temp = nums[position];
+            //put number in the front
+            nums[position] = nums[i];
+            nums[i] = temp;
+            //call permute with position + 1
+            permute(nums, position + 1, result);
+            //put number back to original position
+            nums[i] = nums[position];
+            nums[position] = temp;
+        }
     }
 }
--- src/main/java/Problem2/SortList.java
+++ src/main/java/Problem2/SortList.java
@@ -18,11 +18,62 @@ public class SortList {
 
     public static ListNode findMidAndBreak(ListNode head) {
         // homework
-        return null;
+        if (head == null || head.next == null) {
+            return null;
+        }
+        ListNode p0 = head;
+        ListNode p1 = head;
+        ListNode p2 = head;
+        while (p2 != null && p2.next != null) {
+            p0 = p1;
+            p1 = p1.next;
+            p2 = p2.next.next;
+        }
+        p0.next = null;
+        return p1;
+
     }
 
     public static ListNode mergeLists(ListNode list1, ListNode list2) {
         // homework
-        return null;
+        ListNode list3 = null;
+
+        //if a list is null
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+
+        //if both lists arent null
+        //initial check
+        if (list1.val < list2.val) {
+            list3 = list1;
+            list1 = list1.next;
+        } else {
+            list3 = list2;
+            list2 = list2.next;
+        }
+
+        ListNode curr = list3;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                curr.next = list1;
+                list1 = list1.next;
+            } else {
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            curr = curr.next;
+        }
+        //final check
+        if (list1 == null) {
+            curr.next = list2;
+        } else {
+            curr.next = list1;
+        }
+
+        return list3;
     }
 }
